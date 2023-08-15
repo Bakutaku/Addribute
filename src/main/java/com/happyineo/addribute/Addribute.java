9a52c84635065acde0c94bce83d6ae2feaaf10c6
Bakutaku <133964557+Bakutaku@users.noreply.github.com>
2023-08-15 21:45:55 +0900

    Create Plugin

---
+++ src/main/java/com/happyineo/addribute/Addribute.java
@@ -0,0 +1,17 @@
+package com.happyineo.addribute;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Addribute extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
