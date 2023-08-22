333ad3deae912ce030008c4e319671a2dd726d7c
Bakutaku <133964557+Bakutaku@users.noreply.github.com>
2023-08-22 22:38:53 +0900

    calcDamage()完成

--- src/main/java/com/happyineo/addribute/manager/DamageManager.java
+++ src/main/java/com/happyineo/addribute/manager/DamageManager.java
@@ -1,8 +1,14 @@
 package com.happyineo.addribute.manager;
 
+import com.happyineo.addribute.Beans.StatusConfig;
+import com.happyineo.addribute.StringCalc;
+import org.bukkit.entity.Entity;
+
 public class DamageManager {
 
     private static DamageManager manager;   // 自クラスのインスタンス格納用
+
+    private StatusConfig config = DataManager.getManager().getStatusConfig();   // ステータスのコンフィグ
 
     private DamageManager(){
         manager = this;
@@ -48,9 +54,54 @@ public class DamageManager {
     }
 
 
+    /**
+     * 通常攻撃
+     * @param atk 攻撃者
+     * @param entity 対象
+     * @param attribute 属性
+     * @param damage ダメージ
+     */
+    public void damage(Entity atk,Entity entity,String attribute,double damage){
+
+    }
+
+    /**
+     * 魔法攻撃
+     * @param atk 攻撃者
+     * @param entity 対象
+     * @param attribute 属性
+     * @param damage ダメージ
+     */
+    public void magicDamage(Entity atk,Entity entity,String attribute,double damage){
+
+    }
 
 
+    /**
+     * ダメージ計算
+     * @param damage ダメージ
+     * @param vit 防御力
+     * @param mags 倍率
+     * @return 計算結果
+     */
+    public double calcDamage(double damage,double vit,double mags){
 
+        double answer = 0;  // 結果格納用
+
+        // 計算を行う
+        answer = new StringCalc(config.getCalcDamage())
+                .replace("da",damage)   // 変数変換
+                .replace("vit",vit)
+                .replace("mags",mags)
+                .build()    // 計算式変換
+                .calc();    // 計算
+
+        // 答えが負の値かつ倍率が負の値ではない時、結果を0にする
+        if(answer < 0 && mags >= 0) answer = 0;
+
+        // 結果を返す
+        return answer;
+    }
 
 
 
